9cf6a8de5fb94f3f0a4d12b542fe90a878be5bd9
Steven Senator <sts@lanl.gov>
2017-09-20 17:22:09 -0600

    update to slurm v17, pre-SLUG2017

--- SOURCES/x.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/param.h>
-
-main() {
-	printf ("NBBY: %d\n", NBBY);
-	printf ("sizeof(int) = %d\n", sizeof(int));
-	printf ("sizeof(unsigned int) = %d\n", sizeof(unsigned int));
-	printf ("sizeof(unsigned int) = %d\n", sizeof(unsigned int));
-}
